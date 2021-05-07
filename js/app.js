1bac8423b5fecb493d0e68d2e0d0418c34dc536a
lukson33 <45272632+lukson33@users.noreply.github.com>
2021-05-07 19:57:16 +0200

    structure and styling finished, functionality added for tabs and accordions

--- js/app.js
+++ js/app.js
@@ -1,3 +1,96 @@
+const anchors = document.querySelectorAll(".features__anchor");
+const tabs = document.querySelectorAll(".features__tab");
+const h3s = document.querySelectorAll(".tab__h3");
+const titles = document.querySelectorAll(".question__title");
+const items = document.querySelectorAll(".nav__item");
+const nav = document.getElementsByTagName("nav")[0];
+const links = document.querySelectorAll(".nav__link");
+const hamburger = document.querySelector(".hamburger");
+//make arrays
+const anchorsArr = [...anchors];
+const tabsArr = [...tabs];
+const h3Arr = [...h3s];
+const titlesArr = [...titles];
+const itemsArr = [...items];
+const linksArr = [...links];
+
+anchorsArr.map((anchor) =>
+  anchor.addEventListener("click", (e) => {
+    switch (e.path[0].classList[1]) {
+      case "simple":
+        anchorsArr.map((anchor) => {
+          if (anchor.classList.contains("simple")) {
+            anchor.classList.add("active");
+          } else {
+            anchor.classList.remove("active");
+          }
+        });
+        tabsArr.map((tab) => {
+          if (tab.classList.contains("features__tab--1")) {
+            tab.classList.add("tab-active");
+            tab.classList.remove("tab-hidden");
+          } else {
+            tab.classList.add("tab-hidden");
+            tab.classList.remove("tab-active");
+          }
+        });
+        break;
+      case "speedy":
+        anchorsArr.map((anchor) => {
+          if (anchor.classList.contains("speedy")) {
+            anchor.classList.add("active");
+          } else {
+            anchor.classList.remove("active");
+          }
+        });
+        tabsArr.map((tab) => {
+          if (tab.classList.contains("features__tab--2")) {
+            tab.classList.add("tab-active");
+            tab.classList.remove("tab-hidden");
+          } else {
+            tab.classList.add("tab-hidden");
+            tab.classList.remove("tab-active");
+          }
+        });
+        break;
+      case "easy":
+        anchorsArr.map((anchor) => {
+          if (anchor.classList.contains("easy")) {
+            anchor.classList.add("active");
+          } else {
+            anchor.classList.remove("active");
+          }
+        });
+        tabsArr.map((tab) => {
+          if (tab.classList.contains("features__tab--3")) {
+            tab.classList.add("tab-active");
+            tab.classList.remove("tab-hidden");
+          } else {
+            tab.classList.add("tab-hidden");
+            tab.classList.remove("tab-active");
+          }
+        });
+        break;
+    }
+  })
+);
+
+titlesArr.map((title) =>
+  title.addEventListener("click", () => {
+    title.parentElement.childNodes[3].classList.toggle("question-active");
+    const icon = title.parentElement.childNodes[1].childNodes[3];
+    if (icon.classList.contains("fa-chevron-down")) {
+      icon.classList.remove("fa-chevron-down");
+      icon.classList.add("fa-chevron-up");
+      icon.classList.add("icon-red");
+    } else {
+      icon.classList.add("fa-chevron-down");
+      icon.classList.remove("fa-chevron-up");
+      icon.classList.remove("icon-red");
+    }
+  })
+);
+
 //NAVIGATION
 
 //remove clicks and hover effects on small devices (opacity: 0)
